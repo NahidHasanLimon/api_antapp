ff338c524b3e4f0c9dc82851516fc9c0696eb9fb
Arif.Trinoq <asarifkhan08@gmail.com>
2020-07-24 23:47:12 -0600

    attendace api complete

--- app/Http/Controllers/api/NewAttendanceController.php
+++ app/Http/Controllers/api/NewAttendanceController.php
@@ -32,68 +32,79 @@ class NewAttendanceController extends Controller
 
   public function GetAttendancedaily($user_id,$date)
   {
-
-$attendance_data=Attendance::where('user_id',$user_id)->wheredate('attendance_date',$date)->where('is_approved_s',1)->select('attendance_date','check_in','check_out','status')->get();
+    try{
+      $attendance_data=Attendance::where('user_id',$user_id)->wheredate('attendance_date',$date)->where('is_approved_s',1)->select('attendance_date','check_in','check_out','status')->get();
     
-    foreach($attendance_data as $attendance) {
-      $check_in = new DateTime($attendance->check_in);
-      $check_out = new DateTime($attendance->check_out);
-      $timeDiff = $check_in->diff($check_out);
-      $AttandanceArr = [
-        'inTime' => $attendance->check_in,
-        'outTime' => $attendance->check_out,
-        'duration' => $timeDiff->h . 'h:' . $timeDiff->i . 'm',
-      ];
-
-      $timeArr[] = $timeDiff->h . ':' . $timeDiff->i . ':' . $timeDiff->s;
-      $attenDanceData[] = $AttandanceArr;
+      foreach($attendance_data as $attendance) {
+        $check_in = new DateTime($attendance->check_in);
+        $check_out = new DateTime($attendance->check_out);
+        $timeDiff = $check_in->diff($check_out);
+        $AttandanceArr = [
+          'inTime' => $attendance->check_in,
+          'outTime' => $attendance->check_out,
+          'duration' => $timeDiff->h . 'h:' . $timeDiff->i . 'm',
+        ];
+  
+        $timeArr[] = $timeDiff->h . ':' . $timeDiff->i . ':' . $timeDiff->s;
+        $attenDanceData[] = $AttandanceArr;
+      }
+  
+    $checkIn = $attendance_data[0]->check_in;
+    $checkOut = $attendance_data[0]->check_out;
+    $status =$attendance_data[0]->status;
+  
+    // $d1 = new  DateTime($checkIn);
+    // $d2 = new DateTime($checkOut); 
+    // $Diff = $d1->diff($d2)->h . 'h: ' . $d1->diff($d2)->i. 'm: ' . $d1->diff($d2)->s. 's:';
+    $TotalDurationSum = self::CalculateTime($timeArr);
+  
+  
+         return response()->json([
+  
+                              'success'=>true,
+                              'total_duration'=> $TotalDurationSum,
+                              'initialInTime'=>$checkIn,
+                              'lastOutTime'=>$checkOut,
+                              'status'=>$status,
+                              'attendance_data'=>$attenDanceData,
+                          ],200);
+    }catch(\Exception $e){
+      return response()->json(['success'=>false,'total_duration'=>null,'attendance_date'=>null ],400);
     }
+  
 
-  $checkIn = $attendance_data[0]->check_in;
-  $checkOut = $attendance_data[0]->check_out;
-  $status =$attendance_data[0]->status;
-
-  // $d1 = new  DateTime($checkIn);
-  // $d2 = new DateTime($checkOut); 
-  // $Diff = $d1->diff($d2)->h . 'h: ' . $d1->diff($d2)->i. 'm: ' . $d1->diff($d2)->s. 's:';
-  $TotalDurationSum = self::CalculateTime($timeArr);
-
-
-       return response()->json([
-
-                            'success'=>true,
-                            'total_duration'=> $TotalDurationSum,
-                            'initialInTime'=>$checkIn,
-                            'lastOutTime'=>$checkOut,
-                            'status'=>$status,
-                            'attendance_data'=>$attenDanceData,
-                        ],200);
   }
 
   public function GetAttendanceMonthly($user_id,$start_date,$end_date)
   {
+   try{
+    $attendance_data=Attendance::where('user_id',$user_id)->whereBetween('attendance_date',[$start_date,$end_date])->where('is_approved_s',1)->select('attendance_date','check_in','check_out','status')->get();
    
-     $attendance_data=Attendance::where('user_id',$user_id)->whereBetween('attendance_date',[$start_date,$end_date])->where('is_approved_s',1)->select('attendance_date','check_in','check_out','status')->get();
+    foreach($attendance_data as $attendance){
+          $check_in= new DateTime($attendance->check_in);
+          $check_out= new DateTime($attendance->check_out);
+          $timeDiff = $check_in->diff($check_out);
+          $AttandanceArr = [
+           'attendance_date'=>$attendance->attendance_date,
+           'inTime' => $attendance->check_in,
+           'outTime' => $attendance->check_out,
+           'status' =>$attendance->status,
+           'duration' => $timeDiff->h . 'h:' . $timeDiff->i . 'm:' .$timeDiff->s. 's:' ,
+         ];
    
-     foreach($attendance_data as $attendance){
-           $check_in= new DateTime($attendance->check_in);
-           $check_out= new DateTime($attendance->check_out);
-           $timeDiff = $check_in->diff($check_out);
-           $AttandanceArr = [
-            'attendance_date'=>$attendance->attendance_date,
-            'inTime' => $attendance->check_in,
-            'outTime' => $attendance->check_out,
-            'status' =>$attendance->status,
-            'duration' => $timeDiff->h . 'h:' . $timeDiff->i . 'm:' .$timeDiff->s. 's:' ,
-          ];
-    
-          $timeArr[] = $timeDiff->h . ':' . $timeDiff->i . ':' . $timeDiff->s;
-          $attenDanceData[] = $AttandanceArr;
-     }
-    return response()->json([
+         $timeArr[] = $timeDiff->h . ':' . $timeDiff->i . ':' . $timeDiff->s;
+         $attenDanceData[] = $AttandanceArr;
+    }
+     return response()->json([
 
-      'success'=>true,
-      'attendance_data'=>$attenDanceData,
-  ],200);
+       'success'=>true,
+       'attendance_data'=>$attenDanceData,
+   ],200);
+    
+   }catch (\Exception $e) {
+    return response()->json(['sucess'=>false , 'attendance_data'=>null],400);
+}
+    
+  
   }
 }
